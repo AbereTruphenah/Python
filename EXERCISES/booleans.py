3bc5b91f8727cca966a4edd13c0f3a8080ee0356
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2024-12-03 15:31:34 +0300

    Booleans

---
+++ EXERCISES/booleans.py
@@ -0,0 +1,21 @@
+print(10<3) #False
+print(3>1) #True
+print(6==2) #False
+
+
+a=5
+b=3
+
+if a>b:
+    print(a ,"is greater than",b)
+else:
+    print(a," is less than ",b)    #5 is greter than 3
+
+#Evaluate Values and Variables
+print(bool("hello"))    
+print(bool())
+x=0
+print(bool(x))
+print(bool(""))
+print(bool(True))
+print(bool(False))
