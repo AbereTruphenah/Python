f1d4d18b50a0f913bd2a6d973bc66570a3fecb69
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2024-12-05 00:20:18 +0300

    % string formatting
    String formatting using the % operator

---
+++ EXERCISES/%formatting.py
@@ -0,0 +1,4 @@
+text1="Monday"
+text2="Sunday"
+print("On %s students will start their exams"%text1)
+print("The classes shall start on %s and end on %s because %s is a weekend"%(text1,"Saturday",text2))
