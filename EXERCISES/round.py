426618b7dcf63e4c8696f0a8543b4d061695f19e
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2024-12-07 22:09:34 +0300

    round(), ceil() and floor()

---
+++ EXERCISES/round.py
@@ -0,0 +1,15 @@
+print(round(1.4))
+print(round(1.5))
+print(round(1.6))
+print(round(2.5))
+print(round(2.6))
+
+
+#Math Library
+# #round up to the nearest whole number by using ceil, or down by using floor.
+from math import ceil,floor
+round_up=ceil(12.5)
+print(round_up)
+
+round_down=floor(12.5)
+print(round_down)
