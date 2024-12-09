7c4bffe41f537fc78645b670267c84413e58a038
AbereTruphenah <145029217+AbereTruphenah@users.noreply.github.com>
2024-12-09 22:41:26 +0300

    Unit Convereter

---
+++ EXERCISES/unitconverter.py
@@ -0,0 +1,11 @@
+#print(date.today())
+parsecs=11
+lightyears=parsecs*3.26
+print(str(parsecs)+" parsecs is "+str(lightyears)+" lightyears")
+
+
+#Accepting user input
+parsecs_input=input("Enter number of parsecs: ")
+parsecs=int(parsecs_input)
+lightyears=parsecs*3.26
+print(str(parsecs)+" parsecs is "+str(lightyears)+" lightyears")
